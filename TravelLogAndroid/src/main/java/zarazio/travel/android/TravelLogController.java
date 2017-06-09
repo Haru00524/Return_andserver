1507a287912fbab2351840b21a0f137b0b29e8c7
Haru00524 <akfm00524@gmail.com>
2017-06-10 02:49:18 +0900

    성만
    다시

--- TravelLogAndroid/src/main/java/zarazio/travel/android/TravelLogController.java
+++ TravelLogAndroid/src/main/java/zarazio/travel/android/TravelLogController.java
@@ -7,11 +7,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 import javax.annotation.Resource;
-import javax.inject.Inject;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,116 +20,43 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
-import com.google.gson.Gson;
-
-import zarazio.travel.android.bean.PictureFileDTO;
-import zarazio.travel.android.bean.attachedFileDTO;
-import zarazio.travel.android.bean.boardDTO;
-import zarazio.travel.android.bean.hashTagDTO;
-import zarazio.travel.android.service.ARDataService;
-import zarazio.travel.android.service.boardService;
 
 @Controller
 public class TravelLogController {
-
-	@Resource(name = "uploadPath")
+	
+	@Resource(name="uploadPath")
 	private String uploadPath;
-
-	@Inject
-	private boardService service;
-
-	@RequestMapping(value="/boardList")
-	@ResponseBody
-	public ResponseEntity<String> ARDataList() throws Exception {
-		HttpHeaders resHeaders = new HttpHeaders();
-		resHeaders.add("Content-Type", "application/json;charset=UTF-8");
-
-		List<boardDTO> List = service.boardList();
-
-		Gson gson = new Gson();
-		String data =  gson.toJson(List);
-		
-		System.out.println(data);
-		return new ResponseEntity<String>(data,resHeaders,HttpStatus.CREATED);
-		
-	}
-
-
-	@RequestMapping(value="/insertLog")
-	public ResponseEntity<String> inserLog(HttpServletRequest request) throws UnsupportedEncodingException{
-
-		HttpHeaders resHeaders = new HttpHeaders();
-		resHeaders.add("Content-Type", "application/json;charset=EUC_KR");
-		
-		int board_Type_Code = 0 ;
-		int share_Type = 0;
-		String board_Title = null;
-		String board_Content = null;
-		String hash_Tag = null;
-		double log_longtitude = 0;
-		double log_latitude = 0;
-		String user_id = null;
-		String originalName = "";
-		int file_Type=0;
-		
-		HttpStatus a=HttpStatus.BAD_REQUEST;
-		
+	
+	
+	@RequestMapping("insertLog")
+	public String inserLog(HttpServletRequest request) throws UnsupportedEncodingException{
+		request.setCharacterEncoding("utf-8");
 		FileItemFactory factory = new DiskFileItemFactory();
 		ServletFileUpload upload = new ServletFileUpload(factory);
 		List items= null;
-		
 		try {
 			items = upload.parseRequest(request);
-			a = HttpStatus.OK;
 		} catch (FileUploadException e) {
 			// TODO Auto-generated catch block
-			
 			e.printStackTrace();
 		}
-		Iterator itr = null;
-		if(items!=null){
-		 itr= items.iterator();
-		}
+		Iterator itr = items.iterator();
 	    while (itr.hasNext()) {
 	        FileItem item = (FileItem) itr.next();
 	        System.out.println(item.isFormField());
 	        if (item.isFormField()) { // 파일이 아닌 폼필드에 입력한 내용을 가져옴.
 	          if(item!=null && item.getFieldName().equals("log_Title")) {
-	        	  board_Title = item.getString("EUC_KR");//form field 안에 입력한 데이터를 가져옴
-	        	  System.out.println("로그 제목:"+board_Title+"<br>"); 
+	            String name = item.getString("KSC5601");//form field 안에 입력한 데이터를 가져옴
+	            System.out.println("로그 제목:"+name+"<br>"); 
 	          }else if(item!=null && item.getFieldName().equals("log_Content")) {
-	        	  board_Content = item.getString("EUC_KR");
-	        	  System.out.println("로그 내용:"+board_Content+"<br>");
-	          }else if(item!=null && item.getFieldName().equals("hash_tag")) {
-		          hash_Tag = item.getString("EUC_KR");
-		          System.out.println("해시태그 내용:"+hash_Tag+"<br>");
-		      }else if(item!=null && item.getFieldName().equals("log_longtitude")) {
-		          log_longtitude = Double.parseDouble(item.getString("EUC_KR"));
-		          System.out.println("경도 내용:"+log_longtitude+"<br>");
-		      }else if(item!=null && item.getFieldName().equals("log_latitude")) {
-		          log_latitude = Double.parseDouble(item.getString("EUC_KR"));
-		          System.out.println("위도 내용:"+log_latitude+"<br>");
-		      }else if(item!=null && item.getFieldName().equals("share_type")) {
-		    	  share_Type = Integer.parseInt(item.getString("EUC_KR"));
-		          System.out.println("공유 여부 :"+share_Type+"<br>");
-		      }else if(item!=null && item.getFieldName().equals("board_type")) {
-		    	  board_Type_Code = Integer.parseInt(item.getString("EUC_KR"));
-		          System.out.println("게시판 타입 내용:"+board_Type_Code+"<br>");
-		      }else if(item!=null && item.getFieldName().equals("user_id")) {
-		          user_id = item.getString("EUC_KR");
-		          System.out.println("유저아이디:"+user_id+"<br>");
-		      }else if(item!=null && item.getFieldName().equals("file_Type")) {
-		          file_Type = Integer.parseInt(item.getString("EUC_KR"));
-		          System.out.println("파일 타입:"+file_Type+"<br>");
-		      }
+	            String desc = item.getString("KSC5601");
+	            System.out.println("로그 내용:"+desc+"<br>");
+	          }
 	        }
 	        else{ // 폼 필드가 아니고 파일인 경우
 	            try {
@@ -141,62 +66,19 @@ public class TravelLogController {
 	                if(fileName.equals("null")) continue;
 	                // 전송된 파일을 서버에 저장하기 위한 절차
 	                //String rootPath = getServletContext().getRealPath("/");
-	                originalName = System.currentTimeMillis()+"Travel_log_";
-	                File savedFile = new File("C:/Return_andserver/TravelLogAndroid/src/main/webapp/resources/upload/"+ originalName+fileName); 
+	                File savedFile = new File("C:/Return_andserver/TravelLogAndroid/src/main/webapp/resources/upload/"+System.currentTimeMillis()+"Travel_log_"+fileName); 
 	                item.write(savedFile);// 지정 경로에 파일을 저장함
-	                originalName += fileName;
+	              
 	                System.out.println("<tr><td><b>파일저장 경로:</b></td></tr><tr><td><b>"+savedFile+"</td></tr>");
-	                System.out.println("<tr><td><b><a href=\"DownloadServlet?file="+fileName+"\">"+originalName+"</a></td></tr>");
+	                System.out.println("<tr><td><b><a href=\"DownloadServlet?file="+fileName+"\">"+fileName+"</a></td></tr>");
 	             } catch (Exception e) {
 	            	 System.out.println("서버에 파일 저장중 에러: "+e);
 	               }
 	        }
 	    }
-
-	    int maxboardCode= 0;
-		try {
-			maxboardCode = (service.maxboard_Code()+1);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-       boardDTO board = new boardDTO();
-        
-       	int randomViewY = (int)(Math.random()*1500)-700;
-       	
-        board.setBoard_Code(maxboardCode);
-        board.setBoard_Title(board_Title);
-        board.setBoard_Content(board_Content);
-        board.setBoard_Type_Code(board_Type_Code);
-        board.setLog_latitude(log_latitude);
-        board.setLog_longtitude(log_longtitude);
-        board.setShare_Type(share_Type);
-        board.setUser_id(user_id);
-        board.setRandomViewY(randomViewY);
-	    
-        hashTagDTO hash = new hashTagDTO();
-        hash.setBoard_Code(maxboardCode);
-        hash.setHash_Tag_Content(hash_Tag);
-        
-        attachedFileDTO file = new attachedFileDTO();
-        file.setBoard_Code(maxboardCode);
-        file.setFile_Content(originalName);
-        file.setFile_Type(file_Type);
-        
-        try {
-			service.insertBoard(board);
-        	if(!hash_Tag.equals("")){
-        		service.insertHash(hash);
-        	}
-			if(!originalName.equals("")){
-				service.insertFile(file);
-			}
-        } catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-       
-		return new ResponseEntity<String>("success", resHeaders, a);
+		System.out.println("success");
+		return "success";
 	}
+	
 
 }
--- TravelLogAndroid/src/main/webapp/WEB-INF/views/insertLog.jsp
+++ TravelLogAndroid/src/main/webapp/WEB-INF/views/insertLog.jsp
@@ -1,3 +1,14 @@
-<%
-out.print("sucess");
-%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+</body>
+</html>
